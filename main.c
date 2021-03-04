a85bc4b6da01d0196addece2ff5ad515faaef6a6
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-04 13:58:31 -0600

    Operadores aritemeticos

--- main.c
+++ main.c
@@ -1,20 +1,59 @@
 #include <stdio.h>
 
 int main(void){
-
-    printf("Un mensaje.\n\n");
-
-    printf("%d + ",2);
-    printf("%d = ",3);
+    //Codifique un programa que sume dos enteros
     printf("%d\n\n",2+3);
 
-    printf("%f + ",2.3);
-    printf("%f = ",4.5);
+    //Codifique un programa que sume dos enteros
+    printf("%d\n\n",-2+3);
+
+    //Codifique un programa que sume dos flotantes
     printf("%f\n\n",2.3+4.5);
 
-    printf("%c ",'&');
-    printf("%c ",'$');
-    printf("%c\n\n",'@');
+    //Codifique un programa que sume dos flotantes
+    printf("%f\n\n",-2.3+4.5);
+
+    //Codifique un programa que reste dos enteros
+    printf("%d\n\n",2-3);
+
+    //Codifique un programa que reste dos enteros
+    printf("%d\n\n",-2-3);
+
+    //Codifique un programa que reste dos flotantes
+    printf("%f\n\n",2.3-4.5);
+
+    //Codifique un programa que reste dos flotantes
+    printf("%f\n\n",-2.3-4.5);
+
+    //Codifique un programa que multiplique dos enteros
+    printf("%d\n\n",2*3);
+
+    //Codifique un programa que multiplique dos enteros
+    printf("%d\n\n",-2*3);
+
+    //Codifique un programa que multiplique dos flotantes
+    printf("%f\n\n",2.3*4.5);
+
+    //Codifique un programa que multiplique dos flotantes
+    printf("%f\n\n",-2.3*4.5);
+
+    //Codifique un programa que divida dos enteros
+    printf("%d\n\n",2/3);
+
+    //Codifique un programa que divida dos enteros
+    printf("%d\n\n",-2/3);
+
+    //Codifique un programa que divida dos enteros
+    printf("%f\n\n",2.0/3.0);
+
+    //Codifique un programa que divida dos enteros
+    printf("%f\n\n",-2.0/3.0);
+
+    //Codifique un programa que divida dos flotantes
+    printf("%f\n\n",2.3/4.5);
+
+    //Codifique un programa que divida dos flotantes
+    printf("%f\n\n",-2.3/4.5);
 
     return 0;
 }
