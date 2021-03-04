46b4b19bd4bfcfa2beb49222b080f00e7829586c
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-04 13:42:58 -0600

    Tipos de datos primitivos

--- main.c
+++ main.c
@@ -2,7 +2,19 @@
 
 int main(void){
 
-    printf("Hola, mundo!");
+    printf("Un mensaje.\n\n");
+
+    printf("%d + ",2);
+    printf("%d = ",3);
+    printf("%d\n\n",2+3);
+
+    printf("%f + ",2.3);
+    printf("%f = ",4.5);
+    printf("%f\n\n",2.3+4.5);
+
+    printf("%c ",'&');
+    printf("%c ",'$');
+    printf("%c\n\n",'@');
 
     return 0;
 }
