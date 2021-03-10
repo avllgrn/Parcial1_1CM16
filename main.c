6c37addec2bf6135fcd88190d0fb4768161b1c78
Alfredo Villagran Olguin <avillagran@ipn.mx>
2021-03-10 13:37:02 -0600

    Fahrenheit a Celsius

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 int main(void){
+    //Codifica un programa que convierte de
+    //Fahrenheit a Celsius
 
     float celsius;
     float fahrenheit;
 
-    printf("Ingresa celsius ");
-    scanf("%f",&celsius);
+    printf("Ingresa fahrenheit ");
+    scanf("%f",&fahrenheit);
 
-    fahrenheit = 9.0/5.0 * celsius + 32;
+    celsius = (fahrenheit - 32) * 5.0 / 9.0 ;
 
-    printf("%f C = ",celsius);
-    printf("%f F\n",fahrenheit);
+    printf("%f F = ",fahrenheit);
+    printf("%f C\n",celsius);
 
     return 0;
 }
